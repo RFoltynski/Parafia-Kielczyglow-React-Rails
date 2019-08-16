ad0493835faa885142f8153024ba3001ce009c72
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-16 10:43:42 +0200

    relation between user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: {minimm: 3, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
