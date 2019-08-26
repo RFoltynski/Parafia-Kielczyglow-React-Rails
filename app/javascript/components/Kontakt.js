1e12525db830e1c446ca6608e3af29dc51cf7e6c
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-26 10:39:02 +0200

    react contact form

--- app/javascript/components/Kontakt.js
+++ app/javascript/components/Kontakt.js
@@ -13,32 +13,44 @@ function Kontakt() {
       <div className="overlay">
         <Navbar />
         <center>
-          <h1 class="h1-header"> Kontakt </h1>
+          <h1 className="h1-header"> Kontakt </h1>
 
           <div className="kontakt-row">
             <animated.div style={props}>
               <div className="buttons-box">
-                <h2 name="scroll-to-historia">Dane Kontaktowe</h2>
-                <div className="buttons-content row">
-                  <div className="buttons-box-text col-md-12">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec lacinia hendrerit massa lobortis imperdiet. Duis at
-                    nibh tempor, congue eros dapibus, tristique ante. Vestibulum
-                    ante ipsum primis in faucibus orci luctus et ultrices
-                    posuere cubilia Curae; Donec suscipit, lorem vitae iaculis
-                    scelerisque, orci nulla fermentum nulla, in sollicitudin
-                    velit nisl iaculis sem. Etiam ullamcorper urna eu lectus
-                    efficitur, vehicula faucibus nibh blandit. Ut mattis ac nisi
-                    in convallis. Mauris malesuada mauris nisi, eu aliquet leo
-                    dapibus et. Sed augue ligula, dictum nec condimentum eu,
-                    consequat in ligula. Integer aliquam elementum nisl, ut
-                    bibendum tortor porttitor id. Donec dapibus feugiat ornare.
-                    Integer blandit elementum eros, nec dictum sem hendrerit id.
-                    Phasellus lacus nulla, aliquet a purus quis, porttitor
-                    posuere leo. Nunc a pharetra tortor. Nam tempor iaculis
-                    arcu, et bibendum enim facilisis quis
+                <h2 className="scroll-to-historia">Formularz kontaktowy</h2>
+
+                <form className="offset-md-1 col-md-10 contact-form">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="formGroupExampleInput"
+                      placeholder="Imię i Nazwisko.."
+                    />
                   </div>
-                </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Twój adres email.."
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      placeholder="Twoja wiadomość"
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">
+                    Wyślij
+                  </button>
+                </form>
               </div>
             </animated.div>
           </div>
