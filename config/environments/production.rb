a7e52aac8cfcfecd9fe755b80caee65d17b81bf5
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-26 18:39:38 +0200

    contact form bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,11 +93,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'parafia-kielczyglow.herokuapp.com' }  
-config.action_mailer.delivery_method = :smtp  
-config.action_mailer.perform_deliveries = true  
-config.action_mailer.raise_delivery_errors = false  
-config.action_mailer.default :charset => "utf-8"  
-config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'parafia-kielczyglow.herokuapp.com',
@@ -105,5 +105,4 @@ config.action_mailer.smtp_settings = {
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
-
 end
