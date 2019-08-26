b4a11c3821623da66a80d5a4e5404e9adbcd761d
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-26 10:11:53 +0200

    rails contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :intentions, only: [:index, :show, :edit, :update]
+  resources :contacts, only: [:index, :new, :create]
   root "pages#index"
   get 'kaplani', to: 'pages#index'
   get 'aktualnosci', to: 'pages#index'
