ad47600c1237b9a27bbe6cea77191261474ad47d
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-29 11:28:14 +0200

    franciszek page

--- app/javascript/components/Parafia.js
+++ app/javascript/components/Parafia.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { useSpring, animated } from "react-spring";
+import franciszek from "./img/franciszek.jpg";
+import franciszekM from "./img/franciszek-mlody.jpg";
+import franciszekP from "./img/franciszek-prosto.jpg";
 
 class Parafia extends React.Component {
   constructor() {
@@ -160,29 +163,84 @@ function OPatronie() {
     <div className="parafia-row">
       <animated.div style={props}>
         <div className="buttons-box">
-          <h2 name="scroll-to-historia">O Patronie</h2>
-          <div className="buttons-content row">
+          <h2 name="scroll-to-historia">
+            Franciszek z Asyżu <br /> (1182-1226)
+          </h2>
+          <br />
+          <div className="parafia-row">
             <div className="buttons-box-text col-md-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              lacinia hendrerit massa lobortis imperdiet. Duis at nibh tempor,
-              congue eros dapibus, tristique ante. Vestibulum ante ipsum primis
-              in faucibus orci luctus et ultrices posuere cubilia Curae; Donec
-              suscipit, lorem vitae iaculis scelerisque, orci nulla fermentum
-              nulla, in sollicitudin velit nisl iaculis sem. Etiam ullamcorper
-              urna eu lectus efficitur, vehicula faucibus nibh blandit. Ut
-              mattis ac nisi in convallis. Mauris malesuada mauris nisi, eu
-              aliquet leo dapibus et. Sed augue ligula, dictum nec condimentum
-              eu, consequat in ligula. Integer aliquam elementum nisl, ut
-              bibendum tortor porttitor id. Donec dapibus feugiat ornare.
-              Integer blandit elementum eros, nec dictum sem hendrerit id.
-              Phasellus lacus nulla, aliquet a purus quis, porttitor posuere
-              leo. Nunc a pharetra tortor. Nam tempor iaculis arcu, et bibendum
-              enim facilisis quis
+              Przyszedł na świat w rodzinie zamożnego kupca, sukiennika
+              umbryjskiego Pietro di Bernardo di Moricone. Jego matka, Pika,
+              pochodząca z Pikardii, nadała mu na chrzcie imię Jana (Chrzciciela
+              albo Ewangelisty), które ojciec zmienił na Francesco,
+              prawdopodobnie będąc pod ogromnym wrażeniem Francji, w której
+              zawarł korzystne umowy handlowe. Niektóre źródła podają, że
+              Franciszek miał brata o imieniu Anioł. Pierwsze lata swojego życia
+              spędził w Asyżu, uczęszczał tam do parafialnej szkoły przy
+              kościele św. Jerzego, gdzie zdobył podstawowe wykształcenie[4].
             </div>
-            <div className="col-md-4">
+            <img className="buttons-img col-md-4" src={franciszekP} />
+          </div>
+          <br />
+          <div className="parafia-row">
+            <img className="buttons-img col-md-4" src={franciszekM} />
+            <div className="buttons-box-text col-md-8">
+              Św. Franciszek był zamożnym rycerzem, niestroniącym od dobrej
+              zabawy. Po nawróceniu zamienił swoje bogate ubranie z żebrakiem,
+              modlił się i pokutował. Pewnego dnia usłyszał słowa:{" "}
+              <blockquote> Franciszku napraw mój kościół </blockquote> Zapragnął
+              radykalnie żyć Ewangelią, zachowując ubóstwo i głosząc nawrócenie.
+              Zgromadzili się wokół niego naśladowcy, dla których spisał regułę
+              życia ewangelicznego, dając początek Zakonowi Braci Mniejszych,
+              nazywanych dzisiaj od jego imienia franciszkanami. Franciszek
+              zrozumiał, że celem ubóstwa jest większa miłość, zaś owocem –
+              radość.
+              <br />
+              <br />W jego wspomnienie dziękujemy Panu za dar zbawienia,
+              prosząc, byśmy przyjęli Go tak żarliwie jak Franciszek.
+            </div>
+          </div>
+          <br />
+          <div className="parafia-row-column text-center">
+            <div className="buttons-box-text">
+              <h3 className="h3-header">Modlitwa Świętego Franciszka:</h3>
+            </div>
+            <div className="parafia-row">
+              <blockquote>
+                O Panie, uczyń z nas narzędzia Twojego pokoju,
+                <br />
+                Abyśmy siali miłość tam, gdzie panuje nienawiść;
+                <br />
+                Wybaczenie tam, gdzie panuje krzywda;
+                <br />
+                Jedność tam, gdzie panuje zwątpienie;
+                <br />
+                Nadzieję tam, gdzie panuje rozpacz;
+                <br />
+                Światło tam, gdzie panuje mrok;
+                <br />
+                Radość tam, gdzie panuje smutek.
+                <br />
+                Spraw abyśmy mogli,
+                <br />
+                Nie tyle szukać pociechy, co pociechę dawać;
+                <br />
+                Nie tyle szukać zrozumienia, co rozumieć;
+                <br />
+                Nie tyle szukać miłości, co kochać;
+                <br />
+                Albowiem dając, otrzymujemy;
+                <br />
+                Wybaczając, zyskujemy przebaczenie,
+                <br />
+                A umierając, rodzimy się do wiecznego życia.
+                <br />
+                Przez Chrystusa Pana naszego. Amen.
+              </blockquote>
               <img
-                className="buttons-img"
-                src="..\img\parafia\franciszek.jpg"
+                className="buttons-img col-md-4"
+                src={franciszek}
+                style={{ marginLeft: "auto", marginRight: "auto" }}
               />
             </div>
           </div>
