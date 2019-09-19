80aa1c9561da97daf13b1cac9806efedf0f76dfc
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-19 12:34:49 +0200

    byli proboszczowie added

--- app/javascript/components/kaplani/proboszczowie.jsx
+++ app/javascript/components/kaplani/proboszczowie.jsx
@@ -5,7 +5,7 @@ import Wrobel from "./../img/ksieza/Antoni_Wrobel.JPG";
 import Polak from "./../img/ksieza/Edward_Polak.JPG";
 import Janson from "./../img/ksieza/Józef_Janson.JPG";
 import Kruk from "./../img/ksieza/Wincenty_Kruk.JPG";
-import NieWiem from "./../img/ksieza/IMG_1768.JPG";
+import Turbanski from "./../img/ksieza/Antonii_Turbanski.JPG";
 
 function Proboszczowie() {
   const props = useSpring({
@@ -18,42 +18,42 @@ function Proboszczowie() {
       <animated.div style={props}>
         <div className="buttons-box">
           <h2> Dotychczasowi proboszczowie</h2>
-          <div className="row">
-            <div className="col-md-8">
-              <div>
-                <div>Antonii Wróbel (1925-1938)</div>
-                <img src={Wrobel} />
-              </div>
-              <div>
-                <div>Stanisław Dąbrowski (1938-1939)</div>
-                <img src={NieWiem} />
-              </div>
-              <div>
-                <div>Franciszek Figura (1939-1940)</div>
-                <img src={NieWiem} />
-              </div>
-              <div>
-                <div> Wincenty Kruk (1940-1941) </div>
-                <img src={Kruk} />
-              </div>
-              <div>
-                <div> Józef Janson (1945-1949) (1953-1959) </div>
-                <img src={Janson} />
-              </div>
-              <div>
-                <div> Antoni Turbański (1949-1953) </div>
-                <img src={Kruk} />
-              </div>
-              <div>
-                <div>Edward Polak (1959-1990) </div>
-                <img src={Polak} />
-              </div>
-              <div>
-                <div>Bogdan Ignasiak (1990- do teraz)) </div>
-                <img src={Polak} />
-              </div>
+
+          <div className=" col-md-12 probszczowie text-center">
+            <div>
+              <img src={Wrobel} className="proboszczowie-img" />
+              <div>Antonii Wróbel (1925-1938)</div>
             </div>
-            <div className="col-md-4" />
+            <hr />
+            <div>
+              <img className="proboszczowie-img" />
+              <div>Stanisław Dąbrowski (1938-1939)</div>
+            </div>
+            <hr />
+            <div>
+              <img className="proboszczowie-img" />
+              <div>Franciszek Figura (1939-1940)</div>
+            </div>
+            <hr />
+            <div>
+              <img src={Kruk} className="proboszczowie-img" />
+              <div> Wincenty Kruk (1940-1941) </div>
+            </div>
+            <hr />
+            <div>
+              <img src={Janson} className="proboszczowie-img" />
+              <div> Józef Janson (1945-1949) (1953-1959) </div>
+            </div>
+            <hr />
+            <div>
+              <img src={Turbanski} className="proboszczowie-img" />
+              <div> Antoni Turbański (1949-1953) </div>
+            </div>
+            <hr />
+            <div>
+              <img src={Polak} className="proboszczowie-img" />
+              <div>Edward Polak (1959-1990) </div>
+            </div>
           </div>
         </div>
       </animated.div>
