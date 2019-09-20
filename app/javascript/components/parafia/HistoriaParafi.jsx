e7d329928e8964eb3d77bcac13f09a342bb78232
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-20 15:02:07 +0200

    photo gallery added

--- app/javascript/components/parafia/HistoriaParafi.jsx
+++ app/javascript/components/parafia/HistoriaParafi.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
-
+import ReactBnbGallery from "react-bnb-gallery";
 import caly from "./../img/budowa/caly.JPG";
 import caly2 from "./../img/budowa/caly2.JPG";
 import dach from "./../img/budowa/dach.JPG";
@@ -22,15 +21,46 @@ import gazeta from "./../img/parafia/HistoriaParafi/gazeta.jpg";
 import pamiatka from "./../img/parafia/HistoriaParafi/pamiatka.jpg";
 import ulotka from "./../img/parafia/HistoriaParafi/ulotka.jpg";
 
-function HistoriaParafi() {
-  const props = useSpring({
-    to: { opacity: 1, width: "100%" },
-    from: { opacity: 0 },
-    config: { duration: 500 }
-  });
-  return (
-    <div className="parafia-row">
-      <animated.div style={props}>
+class HistoriaParafi extends React.Component {
+  state = {
+    showPost: false,
+    galleryOpened: false,
+    numberOfPhoto: 0
+  };
+
+  toggleGallery = numberOfPhoto => {
+    this.setState({
+      galleryOpened: !this.state.galleryOpened,
+      numberOfPhoto: numberOfPhoto
+    });
+  };
+
+  photos = [
+    { photo: stary },
+    { photo: gazeta },
+    { photo: pamiatka },
+    { photo: ulotka },
+    { photo: fundamenty },
+    { photo: caly },
+    { photo: caly2 },
+    { photo: dach },
+    { photo: dach2 },
+    { photo: dach3 },
+    { photo: dach4 },
+    { photo: wsrodku },
+    { photo: wsrodku2 },
+    { photo: wsrodku3 },
+    { photo: wsrodku4 },
+    { photo: organy },
+    { photo: stolarka },
+    { photo: konsekracja },
+    { photo: konsekracja2 },
+    { photo: swieto }
+  ];
+
+  render() {
+    return (
+      <div className="parafia-row">
         <div className="buttons-box" style={{ color: "black" }}>
           <h2>Historia Parafii</h2>
           <br />
@@ -42,45 +72,81 @@ function HistoriaParafi() {
               Parafię erygował bp włocławski Stanisław Zdzitkowiecki 21.08.1924
               r.
             </p>
-            <div className="budowa-images">
-              <img src={stary} className="budowa-image" />
-            </div>
+            <ReactBnbGallery
+              show={this.state.galleryOpened}
+              photos={this.photos}
+              onClose={this.toggleGallery}
+              activePhotoIndex={this.state.numberOfPhoto}
+            />
+            {(this.photos || []).map((downloadURL, i) => {
+              if (i < 4) {
+                return (
+                  <img
+                    className="newsView-img"
+                    onClick={() => this.toggleGallery(i)}
+                    src={downloadURL.photo}
+                    key={i}
+                  />
+                );
+              }
+            })}
           </div>
           <div className="text-center offset-md-1 col-md-10">
             <h3 className="h3-header">Budowa Kościoła</h3>
             Obecny kosciół został wybudowany staraniem ks. Józefa Jansona w
             latach 1948-1957.
-            <div className="col-md-12">
-              <div className="row">
-                <img src={fundamenty} className="budowa-image" />
-                <img src={ulotka} className="budowa-image" />
-                <img src={caly} className="budowa-image" />
-                <img src={caly2} className="budowa-image" />
-              </div>
-              <div className="row">
-                <img src={dach} className="budowa-image" />
-                <img src={dach2} className="budowa-image" />
-                <img src={dach3} className="budowa-image" />
-                <img src={dach4} className="budowa-image" />
+            <div className="row">
+              <div className="col-md-12">
+                {(this.photos || []).map((downloadURL, i) => {
+                  if (i > 4 && i <= 10) {
+                    return (
+                      <img
+                        className="newsView-img"
+                        onClick={() => this.toggleGallery(i)}
+                        src={downloadURL.photo}
+                        key={i}
+                      />
+                    );
+                  }
+                })}
               </div>
             </div>
             Prace wewnątrz
-            <div className="budowa-images">
-              <img src={wsrodku} className="budowa-image" />
-              <img src={wsrodku2} className="budowa-image" />
-              <img src={wsrodku3} className="budowa-image" />
-              <img src={wsrodku4} className="budowa-image" />
-              <img src={organy} className="budowa-image" />
-              <img src={stolarka} className="budowa-image" />
+            <div className="row">
+              <div className="col-md-12">
+                {(this.photos || []).map((downloadURL, i) => {
+                  if (i > 10 && i <= 16) {
+                    return (
+                      <img
+                        className="newsView-img"
+                        onClick={() => this.toggleGallery(i)}
+                        src={downloadURL.photo}
+                        key={i}
+                      />
+                    );
+                  }
+                })}
+              </div>
             </div>
           </div>
           <div className="text-center offset-md-1 col-md-10">
             <h3 className="h3-header">Konsekracja Kościoła</h3>
             Konsekrował go bp Zdzisław Goliński 13.06.1957 roku.
-            <div className="budowa-images">
-              <img src={konsekracja} className="budowa-image" />
-              <img src={konsekracja2} className="budowa-image" />
-              <img src={swieto} className="budowa-image" />
+            <div className="row">
+              <div className="col-md-12">
+                {(this.photos || []).map((downloadURL, i) => {
+                  if (i > 16 && i <= 20) {
+                    return (
+                      <img
+                        className="newsView-img"
+                        onClick={() => this.toggleGallery(i)}
+                        src={downloadURL.photo}
+                        key={i}
+                      />
+                    );
+                  }
+                })}
+              </div>
             </div>
           </div>
           <div className="text-center offset-md-1 col-md-10">
@@ -91,9 +157,9 @@ function HistoriaParafi() {
             posœięcił bp Stanisław Nowak 29.05.1988r.
           </div>
         </div>
-      </animated.div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default HistoriaParafi;
