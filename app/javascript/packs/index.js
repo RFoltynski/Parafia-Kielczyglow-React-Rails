5c7c265cf391720bd550d5fd16508aa5d3139000
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-23 12:53:11 +0200

    router code refactoring

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "../components/Router";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./../components/App";
+import Aktualnosci from "./../components/Aktualnosci";
+import Intencje from "./../components/Intencje";
+import Kaplani from "./../components/Kaplani";
+import Kontakt from "./../components/Kontakt";
+import Parafia from "./../components/Parafia";
+import Inne from "./../components/Inne";
+import PageNotFound from "./../components/PageNotFound";
+
+export default class Router extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/aktualnosci" component={Aktualnosci} />
+          <Route path="/intencje" component={Intencje} />
+          <Route path="/kaplani" component={Kaplani} />
+          <Route path="/parafia" component={Parafia} />
+          <Route path="/kontakt" component={Kontakt} />
+          <Route path="/inne" component={Inne} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
