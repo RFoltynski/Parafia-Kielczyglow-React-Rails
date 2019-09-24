d459a15a654ef880307398f53c01ac9b4b5c8c36
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-24 18:59:12 +0200

    domain chnged 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'pl',
+  domain:               'gamil.com',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
