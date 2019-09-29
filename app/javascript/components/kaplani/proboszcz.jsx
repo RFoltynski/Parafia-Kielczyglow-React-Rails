ba6fe4f56bca9627005b824eb1bba7c783dc7a85
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-29 21:58:10 +0200

    proboszcz informations added

--- app/javascript/components/kaplani/proboszcz.jsx
+++ app/javascript/components/kaplani/proboszcz.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
+import proboszcz from "./../img/ksieza/bogdan_ignasiak.jpg";
 
 function Proboszcz() {
   const props = useSpring({
@@ -10,9 +11,10 @@ function Proboszcz() {
   return (
     <div className="inne-row">
       <animated.div style={props}>
-        <div className="buttons-box">
+        <div className="buttons-box text-center">
           <h2>Obecny Proboszcz</h2>
           <h3>Bogdan Ignasiak</h3>
+          <img src={proboszcz} className="proboszczowie-img" />
           <p> Obecny proboszcz parafii kielczyglow pochodzi.</p>
         </div>
       </animated.div>
