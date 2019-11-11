0bd250e08111ec21e265affc2d591d87f2c1eeb6
foltynski.rafal <foltynski.rafal@gmail.com>
2019-11-11 17:46:01 +0100

    content changes
    cut last point from parafia => historia and paste in kosciol filiarny

--- app/javascript/components/parafia/Cmentarz.jsx
+++ app/javascript/components/parafia/Cmentarz.jsx
@@ -19,41 +19,10 @@ function Miejsca() {
           <div className="inne-row">
             <img
               className="buttons-img buttons-img-miejsca col-md-5"
-              src={kaplica}
-            />
-            <div className="buttons-box-text col-md-7 text-center">
-              <h3 className="h3-header">Kościół filialny w Pierzynach</h3>
-              Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
-              Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
-              podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
-              a 20. rokiem życia wstąpił do Kanoników Regularnych Św. Augustyna,
-              którzy mieli swój klasztor na przedmieściu Lizbony.
-            </div>
-          </div>
-          <br />
-          <div className="inne-row">
-            <img
-              className="buttons-img buttons-img-miejsca col-md-5"
               src={cmentarz}
             />
             <div className="buttons-box-text col-md-7  text-center">
               <h3 className="h3-header "> Cmentarz </h3>
-              Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
-              Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
-              podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
-              a 20. rokiem życia wstąpił do Kanoników Regularnych Św. Augustyna,
-              którzy mieli swój klasztor na przedmieściu Lizbony.
-              <br />
-            </div>
-          </div>
-          <br />
-          <div className="inne-row">
-            <img
-              className="buttons-img buttons-img-miejsca col-md-5"
-              src={pomnik}
-            />
-            <div className="buttons-box-text col-md-7  text-center">
-              <h3 className="h3-header "> Pomnik ofiar II wojny światowej </h3>
               Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
               Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
               podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
--- app/javascript/components/parafia/Miejsca.jsx
+++ app/javascript/components/parafia/Miejsca.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 
-import pomnik from "./../img/miejsca/pomnik.jpg";
 import kaplica from "./../img/miejsca/kaplica.JPG";
-import cmentarz from "./../img/miejsca/cmentarz.JPG";
 
 function Miejsca() {
   const props = useSpring({
@@ -23,43 +21,10 @@ function Miejsca() {
             />
             <div className="buttons-box-text col-md-7 text-center">
               <h3 className="h3-header">Kościół filialny w Pierzynach</h3>
-              Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
-              Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
-              podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
-              a 20. rokiem życia wstąpił do Kanoników Regularnych Św. Augustyna,
-              którzy mieli swój klasztor na przedmieściu Lizbony.
-            </div>
-          </div>
-          <br />
-          <div className="inne-row">
-            <img
-              className="buttons-img buttons-img-miejsca col-md-5"
-              src={cmentarz}
-            />
-            <div className="buttons-box-text col-md-7  text-center">
-              <h3 className="h3-header "> Cmentarz </h3>
-              Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
-              Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
-              podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
-              a 20. rokiem życia wstąpił do Kanoników Regularnych Św. Augustyna,
-              którzy mieli swój klasztor na przedmieściu Lizbony.
-              <br />
-            </div>
-          </div>
-          <br />
-          <div className="inne-row">
-            <img
-              className="buttons-img buttons-img-miejsca col-md-5"
-              src={pomnik}
-            />
-            <div className="buttons-box-text col-md-7  text-center">
-              <h3 className="h3-header "> Pomnik ofiar II wojny światowej </h3>
-              Święty Antoni urodził się w roku 1195 w Lizbonie, stolicy
-              Portugalii. Na chrzcie św. otrzymał imię Ferdynand. Według
-              podania, jego rodzicami byli Maria i Marcin Boglioni. Pomiędzy 15.
-              a 20. rokiem życia wstąpił do Kanoników Regularnych Św. Augustyna,
-              którzy mieli swój klasztor na przedmieściu Lizbony.
-              <br />
+              Kościół filialny zbudowany został w latach 1985-1988. Jego budowę
+              rozpoczął miejscowy wikariusz ks. Adam Sołtysiak, dokończył
+              wikariusz, a nastęþnie proboszcz - ks. Bogdan Ignasiak. Uroczyście
+              posœięcił bp Stanisław Nowak 29.05.1988r.
             </div>
           </div>
           <br />
