368356d1df37b53977f7585407acf5852854dd7c
foltynski.rafal <foltynski.rafal@gmail.com>
2019-12-03 22:29:29 +0100

    replace header component to app.js file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./Header";
 import { Link } from "react-router-dom";
 import { animateScroll as scroll, scroller } from "react-scroll";
 import axios from "axios";
@@ -145,6 +144,28 @@ class App extends React.Component {
               </Link>
             </div>
           </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    let link = this.props.link;
+    return (
+      <div className="header" onClick={this.props.scroll}>
+        {" "}
+        <div className="header-box">
+          <h1 className="header-parafia">Parafia Kiełczygłów</h1>
+          <p className="header-parafia-p">
+            <a href="/aktualnosci">Aktualności</a> *{" "}
+            <a href={link.toString()} target="_blank">
+              Intencje
+            </a>{" "}
+            * <a href="/kontakt">Kontakt</a>
+          </p>
+          <div className={"arrow bounce"} />
         </div>
       </div>
     );
