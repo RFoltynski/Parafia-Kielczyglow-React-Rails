7f6c574cc701200736f211ea5e9b1cb5fd3a035b
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-01-17 07:01:30 +0100

    deploy_update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '165.22.19.149', user: 'deploy', roles: %w{app db web}
+server '142.93.106.74', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
