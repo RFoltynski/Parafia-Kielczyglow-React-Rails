b9e75b02fcea428e0d83cf3175c3f88af1dc8459
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-02-07 09:37:52 +0100

    [PK-6] Split "Cmentarz" view on the three tabs "Kaplica", "Cmentarz", "Pogrzeby"

--- app/javascript/components/Cmentarz.js
+++ app/javascript/components/Cmentarz.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
+import CmentarzInfo from "./cmentarz/CmentarzInfo";
+import Kaplica from "./cmentarz/Kaplica";
+import Pogrzeby from "./cmentarz/Pogrzeby";
 class Cmentarz extends Component {
   constructor() {
     super();
@@ -7,29 +10,32 @@ class Cmentarz extends Component {
       render: "",
       buttonPressed1: true,
       buttonPressed2: false,
+      buttonPressed3: false,
     };
   }
   componentWillMount() {
     window.scrollTo(0, 0);
   }
 
-  // handleClick = (compName, e) => {
-  //   this.setState({
-  //     render: compName,
-  //   });
-  // };
+  handleClick = (compName, e) => {
+    this.setState({
+      render: compName,
+    });
+  };
 
-  // _renderSubComp() {
-  //   switch (this.state.render) {
-  //     case "ksiazki":
-  //       return <Ksiazki />;
-  //     case "miejscawsieci":
-  //       return <MiejscaWSieci />;
-  //   }
-  // }
+  _renderSubComp() {
+    switch (this.state.render) {
+      case "cmentarzInfo":
+        return <CmentarzInfo />;
+      case "kaplica":
+        return <Kaplica />;
+      case "pogrzeby":
+        return <Pogrzeby />;
+    }
+  }
 
   onClick1 = (event) => {
-    this.handleClick("ksiazki");
+    this.handleClick("cmentarzInfo");
     (this.buttonPress1 = () => {
       this.setState({
         buttonPressed1: true,
@@ -39,12 +45,22 @@ class Cmentarz extends Component {
     })();
   };
   onClick2 = (event) => {
-    this.handleClick("miejscawsieci");
+    this.handleClick("kaplica");
     (this.buttonPress2 = () => {
       this.setState({
         buttonPressed1: false,
         buttonPressed2: true,
         buttonPressed3: false,
+      });
+    })();
+  };
+  onClick3 = (event) => {
+    this.handleClick("pogrzeby");
+    (this.buttonPress2 = () => {
+      this.setState({
+        buttonPressed1: false,
+        buttonPressed2: false,
+        buttonPressed3: true,
       });
     })();
   };
@@ -56,19 +72,28 @@ class Cmentarz extends Component {
           <h1 className="h1-header"> Cmentarz </h1>
           <div className="cmentarz-buttons">
             <button
-            // onClick={this.onClick1}
-            // className={this.state.buttonPressed1 ? "buttonWhite" : "button"}
+              onClick={this.onClick1}
+              className={this.state.buttonPressed1 ? "buttonWhite" : "button"}
             >
-              Książki
+              Cmentarz
             </button>
             <button
-            // onClick={this.onClick2}
-            // className={this.state.buttonPressed2 ? "buttonWhite" : "button"}
+              onClick={this.onClick2}
+              className={this.state.buttonPressed2 ? "buttonWhite" : "button"}
             >
-              Stony internetowe
+              Kaplica
             </button>
-
-            {/* {this.state.render === "" ? <Ksiazki /> : this._renderSubComp()} */}
+            <button
+              onClick={this.onClick3}
+              className={this.state.buttonPressed3 ? "buttonWhite" : "button"}
+            >
+              Pogrzeby
+            </button>
+            {this.state.render === "" ? (
+              <CmentarzInfo />
+            ) : (
+              this._renderSubComp()
+            )}
           </div>
         </center>
       </div>
