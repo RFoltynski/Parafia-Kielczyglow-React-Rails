7de914245fcd2a64c85b7ead35cd7c2883ab06d0
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-02-09 20:16:05 +0100

    [PK-12]
    Remove stuff from "Powolania w Parafii"

--- app/javascript/components/Duchowienstwo/powolania.jsx
+++ app/javascript/components/Duchowienstwo/powolania.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
-import powolania from "./../img/ksieza/powolania.jpg";
 
 function Powolania() {
   const props = useSpring({
@@ -12,38 +11,13 @@ function Powolania() {
     <div className="inne-row">
       <animated.div style={props}>
         <div className="buttons-box text-center">
-          <h2>Obecny powolania</h2>
-          <h3 className="text-center h3-header">Ks. Bogdan Ignasiak</h3>
-          <div className="ob-powolaniaowie-overlay">
-            <img src={powolania} className="ob-powolaniaowie-img " />
-            <p className="ob-powolaniaowie-text">
-              {" "}
-              <span className={"bold-orange-word"}>Urodzony:</span> <br /> 26
-              października 1984 roku w Rudzie Śląskiej Goduli w rodzinie
-              górniczej. <br />
-              <span className={"bold-orange-word"}>Edukacja:</span> <br />
-              Po ukończeniu szkoły podstawowej w 1999 rok wstąpił do Niższego
-              Seminarium Duchownego Archidiecezji Częstochowskiej, które
-              ukończył egzaminem maturalnym w 2002 roku. <br />
-              Po ukończeniu tej szkoły, rozpocząl studia
-              teologiczno-filozoficzne w Wyższym Seminarium Duchownym
-              Archidiecezji Częstochowskiej, które ukończył w 2010 roku
-              pomyślnie zdanym egzaminem magisterskim z zakresu Historii
-              Kościoła.
-              <br /> <span className={"bold-orange-word"}>Święcenia:</span>{" "}
-              <br />
-              Wyświęcony na kapłana przez Arcybiskupa Metroplitę Stanisława
-              Nowaka 22 maja 2010 roku w Archikatedrze w Częstochowie. <br />{" "}
-              <span className={"bold-orange-word"}>Duszpasterstwo:</span> <br />
-              Pracował jako prefekt w Niższym Seminarium Duchownym : 2010-2012.{" "}
-              <br /> Wikariusz w rodzinnej parafii bp. Stefana Bareły w Kodrębie
-              2012-2016. <br /> Wikariusz w Kiełczygłowie: 2016 do teraz.{" "}
-            </p>
-          </div>
+          <h2>Powołania w Parafii</h2>
+          <h3 className="text-center h3-header">Powołania w Parafii</h3>
+          <div className="ob-powolaniaowie-overlay" />
         </div>
       </animated.div>
     </div>
   );
 }
 
-export default powolania;
+export default Powolania;
