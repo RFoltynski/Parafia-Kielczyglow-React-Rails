6c89b294d98f02dc3d79ab4c021cc5dbe58eae75
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-02-13 11:11:42 +0100

    [PK-27]
    Refactor Parafia tab

--- app/javascript/components/Parafia.js
+++ app/javascript/components/Parafia.js
@@ -6,155 +6,28 @@ import OPatronie from "./parafia/OPatronie.jsx";
 import ParafiaObecnie from "./parafia/ParafiaObecnie";
 import Miejsca from "./parafia/Miejsca.jsx";
 import WspolnotyParafialne from "./parafia/WspolnotyParafialne.jsx";
+import ButtonMenu from "./Elements/ButtonMenu.js";
 
-class Parafia extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      render: "",
-      buttonPressed: true,
-      buttonPressed1: false,
-      buttonPressed2: false,
-      buttonPressed3: false,
-      buttonPressed4: false,
-    };
-  }
+const Parafia = () => {
+  let componentsList = [
+    ["OPatronie", <OPatronie />],
+    ["HistoriaParafi", <HistoriaParafi />],
+    ["Miejsca", <Miejsca />],
+    ["WspolnotyParafialne", <WspolnotyParafialne />],
+    ["ParafiaObecnie", <ParafiaObecnie />],
+  ];
 
-  componentWillMount() {
-    window.scrollTo(0, 0);
-  }
-
-  handleClick = (compName, e) => {
-    this.setState({
-      render: compName,
-    });
-  };
-
-  _renderSubComp() {
-    switch (this.state.render) {
-      case "parafiaObecnie":
-        return <ParafiaObecnie />;
-      case "oPatronie":
-        return <OPatronie />;
-      case "historiaParafi":
-        return <HistoriaParafi />;
-      case "kosciolFiliarny":
-        return <Miejsca />;
-      case "wspolnotyParafialne":
-        return <WspolnotyParafialne />;
-    }
-  }
-
-  onClick = (event) => {
-    this.handleClick("parafiaObecnie");
-    (this.buttonPress = () => {
-      this.setState({
-        buttonPressed: true,
-        buttonPressed1: false,
-        buttonPressed2: false,
-        buttonPressed3: false,
-        buttonPressed4: false,
-      });
-    })();
-  };
-  onClick1 = (event) => {
-    this.handleClick("oPatronie");
-    (this.buttonPress1 = () => {
-      this.setState({
-        buttonPressed: false,
-        buttonPressed1: true,
-        buttonPressed2: false,
-        buttonPressed3: false,
-        buttonPressed4: false,
-      });
-    })();
-  };
-  onClick2 = (event) => {
-    this.handleClick("historiaParafi");
-    (this.buttonPress2 = () => {
-      this.setState({
-        buttonPressed: false,
-        buttonPressed1: false,
-        buttonPressed2: true,
-        buttonPressed3: false,
-        buttonPressed4: false,
-      });
-    })();
-  };
-  onClick3 = (event) => {
-    this.handleClick("kosciolFiliarny");
-    (this.buttonPress3 = () => {
-      this.setState({
-        buttonPressed: false,
-        buttonPressed1: false,
-        buttonPressed2: false,
-        buttonPressed3: true,
-        buttonPressed4: false,
-      });
-    })();
-  };
-  onClick4 = (event) => {
-    this.handleClick("wspolnotyParafialne");
-    (this.buttonPress4 = () => {
-      this.setState({
-        buttonPressed: false,
-        buttonPressed1: false,
-        buttonPressed2: false,
-        buttonPressed3: false,
-        buttonPressed4: true,
-      });
-    })();
-  };
-
-  render() {
-    return (
-      <div className="parafia">
-        <div className="overlay">
-          <Navbar />
-          <center>
-            <h1 className="h1-header"> Parafia </h1>
-            <div className="parafia-buttons">
-              <button
-                onClick={this.onClick}
-                className={this.state.buttonPressed ? "buttonWhite" : "button"}
-              >
-                Nabożeństwa
-              </button>
-              <button
-                onClick={this.onClick1}
-                className={this.state.buttonPressed1 ? "buttonWhite" : "button"}
-              >
-                Patron
-              </button>
-              <button
-                onClick={this.onClick2}
-                className={this.state.buttonPressed2 ? "buttonWhite" : "button"}
-              >
-                Historia
-              </button>
-              <button
-                onClick={this.onClick3}
-                className={this.state.buttonPressed3 ? "buttonWhite" : "button"}
-              >
-                Kościół Filiarny
-              </button>
-              <button
-                onClick={this.onClick4}
-                className={this.state.buttonPressed4 ? "buttonWhite" : "button"}
-              >
-                Wspólnoty Parafialne
-              </button>
-              {this.state.render === "" ? (
-                <ParafiaObecnie />
-              ) : (
-                this._renderSubComp()
-              )}
-            </div>
-          </center>
-        </div>
+  return (
+    <div className="parafia">
+      <div className="overlay">
+        <Navbar />
+        <center>
+          <h1 className="h1-header"> Inne </h1>
+          <ButtonMenu componentsList={componentsList} />
+        </center>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Parafia;
