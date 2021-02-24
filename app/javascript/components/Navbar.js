497a7fe20d26e4948a342abe6564196c01ba793b
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-02-24 22:02:25 +0100

    [PK-42]
    Implementing mobile first aproch in navbar component

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -64,100 +64,98 @@ class Navbar extends Component {
       "pl/inne",
     ];
     return (
-      <div>
-        <div className={this.state.visible ? "navbar-row" : "navbar--hidden"}>
-          <div className="navbar-logo">
-            {" "}
-            <a className="navbar-logo-link" href="/">
-              Parafia Kiełczygłów
-            </a>
-          </div>
-          <div className="navbar-menu">
-            <button
-              className="navbar-menu-button"
-              onClick={this.dropMenu}
-              style={
-                this.state.dropMenu
-                  ? {
-                      position: "fixed",
-                      top: "0px",
-                      right: "0px",
-                    }
-                  : {}
-              }
-            >
-              <img className="navbar-menu-img" src={Button} />
-            </button>
-          </div>
-          <div
-            className={
+      <div className={this.state.visible ? "navbar-row" : "navbar--hidden"}>
+        <div className="navbar-logo">
+          {" "}
+          <a className="navbar-logo-link" href="/">
+            Parafia Kiełczygłów
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <button
+            className="navbar-menu-button"
+            onClick={this.dropMenu}
+            style={
               this.state.dropMenu
-                ? "navbar-links navbar-links-drop-show"
-                : "navbar-links"
+                ? {
+                    position: "fixed",
+                    top: "0px",
+                    right: "0px",
+                  }
+                : {}
             }
           >
-            <a
-              className={
-                this.state.currentLocation.includes(location[0])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/Ogloszenia"
-            >
-              Ogłoszenia
-            </a>
-            <a
-              className={
-                this.state.currentLocation.includes(location[1])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/cmentarz"
-            >
-              Cmentarz
-            </a>
-            <a
-              className={
-                this.state.currentLocation.includes(location[2])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/parafia"
-            >
-              {" "}
-              Parafia
-            </a>
-            <a
-              className={
-                this.state.currentLocation.includes(location[3])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/Duchowienstwo"
-            >
-              Duchowieństwo
-            </a>
-            <a
-              className={
-                this.state.currentLocation.includes(location[4])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/kontakt"
-            >
-              Kontakt
-            </a>
-            <a
-              className={
-                this.state.currentLocation.includes(location[5])
-                  ? "navbar-link-active"
-                  : "navbar-link"
-              }
-              href="/inne"
-            >
-              Inne
-            </a>
-          </div>
+            <img className="navbar-menu-img" src={Button} />
+          </button>
+        </div>
+        <div
+          className={
+            this.state.dropMenu
+              ? "navbar-links navbar-links-drop-show"
+              : "navbar-links"
+          }
+        >
+          <a
+            className={
+              this.state.currentLocation.includes(location[0])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/Ogloszenia"
+          >
+            Ogłoszenia
+          </a>
+          <a
+            className={
+              this.state.currentLocation.includes(location[1])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/cmentarz"
+          >
+            Cmentarz
+          </a>
+          <a
+            className={
+              this.state.currentLocation.includes(location[2])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/parafia"
+          >
+            {" "}
+            Parafia
+          </a>
+          <a
+            className={
+              this.state.currentLocation.includes(location[3])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/Duchowienstwo"
+          >
+            Duchowieństwo
+          </a>
+          <a
+            className={
+              this.state.currentLocation.includes(location[4])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/kontakt"
+          >
+            Kontakt
+          </a>
+          <a
+            className={
+              this.state.currentLocation.includes(location[5])
+                ? "navbar-link-active"
+                : "navbar-link"
+            }
+            href="/inne"
+          >
+            Inne
+          </a>
         </div>
       </div>
     );
