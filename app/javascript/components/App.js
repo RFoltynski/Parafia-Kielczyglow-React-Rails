101e75996c8a15539fd78c97caffec2ad77888b8
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-03-13 17:30:06 +0100

    PK-58
    Redesign of main page, mobile first approch

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -10,79 +10,32 @@ import inne from "./img/kafelki/inne.JPG";
 class App extends React.Component {
   render() {
     return (
-      <div className="boxes-container">
-        <div className="logo"> Parafia Kiełczygłów</div>
-        <div className="boxes-rows">
-          <div className="boxes-row">
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link image-fix" to="/Ogloszenia">
-                  <h2 className="box-overlay-h2">Ogłoszenia</h2>
-                  <img src={Ogloszenia} />
-                </Link>
-                <Link to="/Ogloszenia">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+      <div className="main-page-container">
+        <div className="header">
+          {" "}
+          Parafia Katolicka pw. Świętego Antoniego w Kiełczygłowie
+        </div>
+        <div className="pills-container">
+          <div className="pills-row">
+            <div className="pill">
+              <a href="/Ogloszenia">Ogszenia</a>
             </div>
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link" to="/cmentarz">
-                  <h2 className="box-overlay-h2">Cmentarz</h2>
-                  <img src={cmentarz} />
-                </Link>
-
-                <Link to="/cmentarz">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+            <div className="pill">
+              <a href="/cmentarz">Cmentarz</a>
             </div>
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link" to="/parafia">
-                  <h2 className="box-overlay-h2">Parafia</h2>
-                  <img src={parafia} />
-                </Link>
-
-                <Link to="/parafia">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+            <div className="pill">
+              <a href="/parafia"> Parafia </a>
             </div>
           </div>
-          <div className="boxes-row">
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link" to="/Duchowienstwo">
-                  <h2 className="box-overlay-h2">Duchowieństwo</h2>
-                  <img src={Duchowienstwo} />
-                </Link>
-                <Link to="/Duchowienstwo">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+          <div className="pills-row">
+            <div className="pill">
+              <a href="/Duchowienstwo">Duchowienstwo</a>
             </div>
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link" to="/kontakt">
-                  <h2 className="box-overlay-h2">Kontakt</h2>
-                  <img src={kontakt} />
-                </Link>
-                <Link to="/kontakt">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+            <div className="pill">
+              <a href="/kontakt">Kontakt</a>
             </div>
-            <div className="row-item">
-              <div className="box-overlay">
-                <Link className="box-overlay-link" to="/inne">
-                  <h2 className="box-overlay-h2">Inne</h2>
-                  <img src={inne} />
-                </Link>
-                <Link to="/inne">
-                  <p className="box-overlay-p" />
-                </Link>
-              </div>
+            <div className="pill">
+              <a href="/inne">Inne</a>
             </div>
           </div>
         </div>
