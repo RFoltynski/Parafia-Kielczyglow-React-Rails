c08d73d4b4ae9c8c1ff1b1ea7db60b1f7db333fb
Rafal Foltynski <Foltynski.Rafal@gmail.com>
2021-03-14 11:27:09 +0100

    PK-58
    Updating main page in mobile version

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -14,30 +14,38 @@ class App extends React.Component {
         <div className="pills-container">
           <div className="pills-row">
             <Link className="pill" to="/Ogloszenia">
-              <i class="fas fa-scroll" />
+              <i class="fas fa-scroll pill-icon" />
               <span className="pill-name">Ogloszenia</span>
+              <span className="pill-description">Aktualnosci, Intencje</span>
             </Link>
             <Link className="pill" to="/cmentarz">
-              <i class="fas fa-cross" />
+              <i class="fas fa-cross pill-icon" />
               <span className="pill-name">Cmentarz</span>
+              <span className="pill-description">
+                Pogrzeby, Kaplica, Cmentarz
+              </span>
             </Link>
             <Link className="pill" to="/parafia">
-              <i class="fas fa-map-marked" />{" "}
+              <i class="fas fa-map-marked pill-icon" />{" "}
               <span className="pill-name">Parafia</span>
+              <span className="pill-description">Informacje ogolne</span>
             </Link>
           </div>
           <div className="pills-row">
             <Link className="pill" to="/Duchowienstwo">
-              <i class="fas fa-chess-king" />
-              <span className="pill-name">Duchowienstwo</span>
+              <i class="fas fa-chess-king pill-icon" />
+              <span className="pill-name">Kaplani</span>
+              <span className="pill-description">Osoby duchowne</span>
             </Link>
             <Link className="pill" to="/kontakt">
-              <i class="fas fa-phone" />
+              <i class="fas fa-phone pill-icon" />
               <span className="pill-name">Kontakt</span>
+              <span className="pill-description">Dane kontaktowe</span>
             </Link>
             <Link className="pill" to="/inne">
-              <i class="fas fa-people-carry" />
+              <i class="fas fa-people-carry pill-icon" />
               <span className="pill-name">Wspolnota</span>
+              <span className="pill-description">Informacje o wspolnotach</span>
             </Link>
           </div>
         </div>
