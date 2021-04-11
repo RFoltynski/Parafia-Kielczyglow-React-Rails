cbe4fc83936f392d922912b19ce60570c739e5cd
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-11 12:22:35 +0200

    PK-60
    - Basic implementation of redux structure
    - Create store
    - Create rootReducer
    - Implementation of middlewares
    - Add posts action and reducer file

---
+++ app/javascript/redux/posts/post.reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  defaultPost: "no posts yet",
+};
+
+const postReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
