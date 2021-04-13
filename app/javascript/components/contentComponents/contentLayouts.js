57703d32b0efc9a9b26ec964d580846247472d4b
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-13 10:18:34 +0200

    PK-64
    - Add multiple images content component

--- app/javascript/components/contentComponents/contentLayouts.js
+++ app/javascript/components/contentComponents/contentLayouts.js
@@ -16,3 +16,32 @@ export const Basic = (props) => {
     </section>
   );
 };
+
+export const ManyImages = (props) => {
+  const { header, description, images, reverse } = props.data;
+  const [name, changeName] = useState("");
+  useEffect(() => {
+    if (reverse) changeName(name + " reverse");
+  }, []);
+
+  return (
+    <section className={"section manyimages" + name}>
+      <div className="section_text manyimages">
+        <h4 className="section_text_header manyimages">{header}</h4>
+        <p className="section_text_content manyimages">{description}</p>
+      </div>
+      <div className={"section_images"}>
+        {images.map((image, key) => {
+          return (
+            <img
+              src={image}
+              key={key}
+              alt={"sw.franciszek" + key}
+              className={"section_image manyimages"}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
--- app/javascript/components/parafia/OPatronie.jsx
+++ app/javascript/components/parafia/OPatronie.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import franciszek from "./../img/parafia/Opatronie/franciszek.jpg";
 import franciszekM from "./../img/parafia/Opatronie/franciszek-mlody.jpg";
 import franciszekP from "./../img/parafia/Opatronie/franciszek-prosto.jpg";
-import { Basic } from "../contentComponents/contentLayouts";
+import { Basic, ManyImages } from "../contentComponents/contentLayouts";
 
 function OPatronie() {
   const section1 = {
@@ -10,7 +10,15 @@ function OPatronie() {
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit saepe a voluptatem blanditiis tempore ipsa dolore, amet porro minus quos ea illum eaque consequuntur molestias iusto corrupti sint facilis nam!",
     image: franciszek,
-    reverse: true,
+    reverse: false,
+  };
+
+  const section2 = {
+    header: "Życie",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit saepe a voluptatem blanditiis tempore ipsa dolore, amet porro minus quos ea illum eaque consequuntur molestias iusto corrupti sint facilis nam!",
+    images: [franciszek, franciszekM, franciszekP],
+    reverse: false,
   };
 
   return (
@@ -19,6 +27,7 @@ function OPatronie() {
         <h2>Św. Antoni Padewski</h2>
         <div className="subcategory__view__column">
           <Basic data={section1} />
+          <ManyImages data={section2} />
         </div>
       </div>
     </div>
