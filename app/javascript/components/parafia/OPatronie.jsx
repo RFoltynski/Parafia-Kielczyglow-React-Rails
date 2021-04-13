9e224e06f78483436bb4840967de18e5763753e1
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-13 07:22:48 +0200

    PK-64
    - Add basic content structure

--- app/javascript/components/parafia/OPatronie.jsx
+++ app/javascript/components/parafia/OPatronie.jsx
@@ -9,9 +9,36 @@ function OPatronie() {
       <div className="subcategory__view">
         <h2>Św. Antoni Padewski</h2>
         <div className="subcategory__view__column">
-          <img src={franciszek} alt="sw.franciszek" class="images-desktop" />
-          <img src={franciszekM} alt="sw.franciszek" class="images-desktop" />
-          <img src={franciszekP} alt="sw.franciszek" class="images-desktop" />
+          <section className={"section"}>
+            <img
+              src={franciszek}
+              alt="sw.franciszek"
+              className="sections_image"
+            />
+            <div className="section_text">
+              <h4 className="section_text_header">Życie</h4>
+              <p className="section_text_content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Perspiciatis distinctio quibusdam culpa accusamus error
+                repudiandae totam labore, perferendis beatae, possimus neque!
+                Sequi rem porro natus harum eum officiis tempora ut!
+              </p>
+            </div>
+          </section>
+          <section className={"section"}>
+            <img
+              src={franciszekM}
+              alt="sw.franciszek"
+              className="sections_image"
+            />
+          </section>
+          <section className={"section"}>
+            <img
+              src={franciszekP}
+              alt="sw.franciszek"
+              className="sections_image"
+            />
+          </section>
         </div>
       </div>
     </div>
