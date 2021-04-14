be5a7eb9cf39930a535115fd18116f3f3e592187
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-14 07:32:23 +0200

    PK-66
    - Changess in folder structure

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,19 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import App from "./../components/App";
-import Ogloszenia from "./../components/Ogloszenia";
-import Duchowienstwo from "./../components/Duchowienstwo";
-import Kontakt from "./../components/Kontakt";
-import Parafia from "./../components/Parafia";
-import Inne from "./../components/Inne";
-import PageNotFound from "../components/elements/PageNotFound";
-import Cmentarz from "../components/Cmentarz";
+import App from "./components/App";
+import Ogloszenia from "./components/Ogloszenia";
+import Duchowienstwo from "./components/Duchowienstwo";
+import Kontakt from "./components/Kontakt";
+import Parafia from "./components/Parafia";
+import Inne from "./components/Inne";
+import PageNotFound from "./components/elements/PageNotFound";
+import Cmentarz from "./components/Cmentarz";
+import Main from "./components/Main";
+
+import CmentarzInfo from "./components/subcomponents/cmentarz/CmentarzInfo";
+import Kaplica from "./components/subcomponents/cmentarz/Kaplica";
+import Pogrzeby from "./components/subcomponents/cmentarz/Pogrzeby";
 
 import { Provider } from "react-redux";
-import store from "../redux/store";
+import store from "./redux/store";
 
 export default class Router extends React.Component {
+  cmentarz = () => {
+    return (
+      <Main
+        componentsList={[
+          ["Cmentarz", <CmentarzInfo key={1} />],
+          ["Kaplica", <Kaplica key={2} />],
+          ["Pogrzeby", <Pogrzeby key={3} />],
+        ]}
+        header={"cmentarz"}
+      />
+    );
+  };
+
   render() {
     return (
       <Provider store={store}>
@@ -21,7 +39,7 @@ export default class Router extends React.Component {
           <Switch>
             <Route exact path="/" component={App} />
             <Route path="/Ogloszenia" component={Ogloszenia} />
-            <Route path="/cmentarz" component={Cmentarz} />
+            <Route path="/cmentarz" component={this.cmentarz} />
             <Route path="/Duchowienstwo" component={Duchowienstwo} />
             <Route path="/parafia" component={Parafia} />
             <Route path="/kontakt" component={Kontakt} />
