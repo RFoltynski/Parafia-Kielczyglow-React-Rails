ce2e524b11fccd26afd931b281a4afb6c0c98de6
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-14 08:13:07 +0200

    PK-66
    - Main components refactoring
    - Creating one file componentsList where components are returned from mainComponent
    - removing files

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,48 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./components/App";
-import Ogloszenia from "./components/Ogloszenia";
-import Duchowienstwo from "./components/Duchowienstwo";
-import Kontakt from "./components/Kontakt";
-import Parafia from "./components/Parafia";
-import Inne from "./components/Inne";
 import PageNotFound from "./components/elements/PageNotFound";
-import Cmentarz from "./components/Cmentarz";
-import Main from "./components/Main";
-
-import CmentarzInfo from "./components/subcomponents/cmentarz/CmentarzInfo";
-import Kaplica from "./components/subcomponents/cmentarz/Kaplica";
-import Pogrzeby from "./components/subcomponents/cmentarz/Pogrzeby";
+import {
+  cmentarz,
+  duchowienstwo,
+  inne,
+  ogloszenia,
+  kontakt,
+  parafia,
+} from "./components/ComponentsList";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 export default class Router extends React.Component {
-  cmentarz = () => {
-    return (
-      <Main
-        componentsList={[
-          ["Cmentarz", <CmentarzInfo key={1} />],
-          ["Kaplica", <Kaplica key={2} />],
-          ["Pogrzeby", <Pogrzeby key={3} />],
-        ]}
-        header={"cmentarz"}
-      />
-    );
-  };
-
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={App} />
-            <Route path="/Ogloszenia" component={Ogloszenia} />
-            <Route path="/cmentarz" component={this.cmentarz} />
-            <Route path="/Duchowienstwo" component={Duchowienstwo} />
-            <Route path="/parafia" component={Parafia} />
-            <Route path="/kontakt" component={Kontakt} />
-            <Route path="/inne" component={Inne} />
+            <Route path="/Ogloszenia" component={ogloszenia} />
+            <Route path="/cmentarz" component={cmentarz} />
+            <Route path="/Duchowienstwo" component={duchowienstwo} />
+            <Route path="/parafia" component={parafia} />
+            <Route path="/kontakt" component={kontakt} />
+            <Route path="/inne" component={inne} />
             <Route component={PageNotFound} />
           </Switch>
         </BrowserRouter>
