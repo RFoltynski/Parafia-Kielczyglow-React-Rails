979a0f60e53cbccdc2a789342ea5b2384af50204
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-15 09:01:07 +0200

    PK-69
    - fix for background issue

--- app/javascript/packs/components/elements/Navbar.js
+++ app/javascript/packs/components/elements/Navbar.js
@@ -68,104 +68,100 @@ const Navbar = () => {
           <div className="navbar-logo-link"> Parafia-Kielczyglow.pl</div>
         </div>
       ) : (
-        <div>
-          <div style={{ ...navbarStyles }}>
-            <div className="navbar-logo">
-              {" "}
-              <Link className="navbar-logo-link" to="/">
-                Parafia-Kielczyglow.pl
-              </Link>
-            </div>
-            <div className="navbar-menu">
-              <button
-                className="navbar-menu-button"
-                onClick={dropMenuHnadler}
-                style={
-                  dropMenu
-                    ? {
-                        position: "fixed",
-                        top: "0px",
-                        right: "0px",
-                      }
-                    : {}
-                }
-              >
-                <img className="navbar-menu-img" src={Button} />
-              </button>
-            </div>
-            <div
-              className={
+        <div style={{ ...navbarStyles }}>
+          <div className="navbar-logo">
+            {" "}
+            <Link className="navbar-logo-link" to="/">
+              Parafia-Kielczyglow.pl
+            </Link>
+          </div>
+          <div className="navbar-menu">
+            <button
+              className="navbar-menu-button"
+              onClick={dropMenuHnadler}
+              style={
                 dropMenu
-                  ? "navbar-links navbar-links-drop-show"
-                  : "navbar-links"
+                  ? {
+                      position: "fixed",
+                      top: "0px",
+                      right: "0px",
+                    }
+                  : {}
               }
             >
-              <Link
-                className={
-                  currentLocation.includes(location[0])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/Ogloszenia"
-              >
-                Ogłoszenia
-              </Link>
+              <img className="navbar-menu-img" src={Button} />
+            </button>
+          </div>
+          <div
+            className={
+              dropMenu ? "navbar-links navbar-links-drop-show" : "navbar-links"
+            }
+          >
+            <Link
+              className={
+                currentLocation.includes(location[0])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/Ogloszenia"
+            >
+              Ogłoszenia
+            </Link>
 
-              <Link
-                className={
-                  currentLocation.includes(location[1])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/cmentarz"
-              >
-                Cmentarz
-              </Link>
+            <Link
+              className={
+                currentLocation.includes(location[1])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/cmentarz"
+            >
+              Cmentarz
+            </Link>
 
-              <Link
-                className={
-                  currentLocation.includes(location[2])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/parafia"
-              >
-                {" "}
-                Parafia
-              </Link>
+            <Link
+              className={
+                currentLocation.includes(location[2])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/parafia"
+            >
+              {" "}
+              Parafia
+            </Link>
 
-              <Link
-                className={
-                  currentLocation.includes(location[3])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/Duchowienstwo"
-              >
-                Duchowieństwo
-              </Link>
+            <Link
+              className={
+                currentLocation.includes(location[3])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/Duchowienstwo"
+            >
+              Duchowieństwo
+            </Link>
 
-              <Link
-                className={
-                  currentLocation.includes(location[4])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/kontakt"
-              >
-                Kontakt
-              </Link>
-              <Link
-                className={
-                  currentLocation.includes(location[5])
-                    ? "navbar-link-active"
-                    : "navbar-link"
-                }
-                to="/inne"
-              >
-                Wspólnoty
-              </Link>
-            </div>
+            <Link
+              className={
+                currentLocation.includes(location[4])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/kontakt"
+            >
+              Kontakt
+            </Link>
+            <Link
+              className={
+                currentLocation.includes(location[5])
+                  ? "navbar-link-active"
+                  : "navbar-link"
+              }
+              to="/inne"
+            >
+              Wspólnoty
+            </Link>
           </div>
         </div>
       )}
