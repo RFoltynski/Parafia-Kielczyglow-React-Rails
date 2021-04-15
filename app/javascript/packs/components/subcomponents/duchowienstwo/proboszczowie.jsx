886c9bf0674fb9bd22c6288c5bfbc5b1f5a5ccec
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-15 08:41:22 +0200

    PK-69
    - Remove unused imports

--- app/javascript/packs/components/subcomponents/duchowienstwo/proboszczowie.jsx
+++ app/javascript/packs/components/subcomponents/duchowienstwo/proboszczowie.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
-
-import Wrobel from "./../../img/ksieza/Antoni_Wrobel.JPG";
-import Polak from "./../../img/ksieza/Edward_Polak.JPG";
 import Janson from "./../../img/ksieza/Józef_Janson.JPG";
 import Kruk from "./../../img/ksieza/Wincenty_Kruk.JPG";
+import Polak from "./../../img/ksieza/Edward_Polak.JPG";
+import React from "react";
 import Turbanski from "./../../img/ksieza/Antonii_Turbanski.JPG";
+import Wrobel from "./../../img/ksieza/Antoni_Wrobel.JPG";
 
 function Proboszczowie() {
   return (
