b2605d677a7b333ea048a22329251fc5ee71f7ea
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-18 12:58:36 +0200

    PK-74
    - Add bio component to layoutscomponents

--- app/javascript/packs/components/elements/contentLayouts.js
+++ app/javascript/packs/components/elements/contentLayouts.js
@@ -72,3 +72,50 @@ export const LayoutContent = (props) => {
     </div>
   );
 };
+
+export const LayoutSubContent = (props) => {
+  return (
+    <div className={"section_text " + props.class}>
+      <h5 className={"section_text_header " + props.class}>{props.header}</h5>
+      <p className={"section_text_content " + props.class}>
+        {props.description}
+      </p>
+    </div>
+  );
+};
+
+export const Bio = (props) => {
+  const { header, description, subcontent, image, reverse } = props.data;
+  return (
+    <section className={"section " + "bio"}>
+      <Gallery>
+        <Item original={image} thumbnail={image} width="1366" height="868">
+          {({ ref, open }) => (
+            <img
+              ref={ref}
+              onClick={open}
+              src={image}
+              className={"section_image bio"}
+            />
+          )}
+        </Item>
+      </Gallery>
+      <div>
+        <LayoutContent
+          header={header}
+          description={description}
+          class={"bio"}
+        />
+        {subcontent.map((subcontent) => {
+          return (
+            <LayoutSubContent
+              header={subcontent.header}
+              description={subcontent.description}
+              class={"subcontent bio"}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
