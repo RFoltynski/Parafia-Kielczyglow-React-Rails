dc96d1b4e5fc0b9423e8d479df93a3f5d5384dcb
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-22 07:03:20 +0200

    PK-76
    - fix in Sakramenty component

--- app/javascript/packs/components/subcomponents/informacje/Sakramenty.jsx
+++ app/javascript/packs/components/subcomponents/informacje/Sakramenty.jsx
@@ -1,22 +1,18 @@
-import {
-  LayoutContentMany,
-  LayoutContentWithSubcontent,
-} from "./../../elements/contentLayouts";
-
-import { LayoutSubContent } from "./../../elements/content/textContent";
+import { LayoutContentWithSubcontent } from "./../../elements/contentLayouts";
 import MainSubcomponent from "../mainSubcomponent";
 import React from "react";
+import { Subcontent } from "./../../elements/content/textContent";
 import { useSelector } from "react-redux";
 
 export const Sakramenty = (props) => {
   const data = useSelector((state) => state.sakramenty);
   const subcomponentsContent = [
-    <LayoutSubContent data={data.chrzest} />,
+    <Subcontent data={data.chrzest} />,
     <LayoutContentWithSubcontent data={data.bierzmowanie} />,
-    <LayoutSubContent data={data.komunia} />,
-    <LayoutSubContent data={data.namaszczenieChorych} />,
-    <LayoutSubContent data={data.pogrzeb} />,
-    <LayoutSubContent data={data.slub} />,
+    <Subcontent data={data.komunia} />,
+    <Subcontent data={data.namaszczenieChorych} />,
+    <Subcontent data={data.pogrzeb} />,
+    <Subcontent data={data.slub} />,
   ];
   return (
     <MainSubcomponent layouts={subcomponentsContent} header={"Sakramenty"} />
