84dd29450a20d5412d2b5cd20e8ec3706ffdac5f
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-23 05:55:40 +0200

    PK-78
    - Refactoring in 'OPatronie'

--- app/javascript/packs/components/subcomponents/parafia/OPatronie.jsx
+++ app/javascript/packs/components/subcomponents/parafia/OPatronie.jsx
@@ -7,11 +7,15 @@ import { useSelector } from "react-redux";
 export const OPatronie = () => {
   const data = useSelector((state) => state.oPatronie);
 
-  const subcomponentsContent = [
-    <Basic data={data.section1} key={1} />,
-    <ManyImages data={data.section2} key={2} />,
-    <Basic data={data.section3} key={3} />,
-  ];
+  const keys = Object.keys(data);
+
+  const subcomponentsContent = keys.map((item, key) => {
+    if (item === "section2") {
+      return <ManyImages data={data[item]} key={key} />;
+    } else {
+      return <Basic data={data[item]} key={key} />;
+    }
+  });
   return (
     <MainSubcomponent layouts={subcomponentsContent} header={"O patronie"} />
   );
