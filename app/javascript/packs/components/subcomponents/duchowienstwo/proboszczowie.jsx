717c5b6bdacf426f111303132524f9e2465a4f77
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-23 07:49:35 +0200

    PK-78
    - Refactoring in 'Proboszczowie' subcomponent

--- app/javascript/packs/components/subcomponents/duchowienstwo/proboszczowie.jsx
+++ app/javascript/packs/components/subcomponents/duchowienstwo/proboszczowie.jsx
@@ -1,18 +1,10 @@
-import { Bio } from "./../../elements/content/Bio";
 import MainSubcomponent from "./../mainSubcomponent";
 import React from "react";
-import { useSelector } from "react-redux";
 
 export const Proboszczowie = () => {
-  const data = useSelector((state) => state.proboszczowie);
-  const keys = Object.keys(data);
-
-  const subcomponentsContent = keys.map((item, key) => {
-    return <Bio data={data[item]} key={key} />;
-  });
   return (
     <MainSubcomponent
-      layouts={subcomponentsContent}
+      reducerProperty={"proboszczowie"}
       header={"Byli Proboszczowie"}
     />
   );
--- app/javascript/packs/redux/duchowienstwo/proboszczowie.reducer.js
+++ app/javascript/packs/redux/duchowienstwo/proboszczowie.reducer.js
@@ -5,6 +5,7 @@ import Turbanski from "./../../components/img/ksieza/Antonii_Turbanski.JPG";
 import Wrobel from "./../../components/img/ksieza/Antoni_Wrobel.JPG";
 const INITIAL_STATE = {
   polak: {
+    component: "Bio",
     images: [Polak],
     header: "Ks. Edward Polak (1959-1990)",
     description:
@@ -18,6 +19,7 @@ const INITIAL_STATE = {
     reverse: false,
   },
   turbanski: {
+    component: "Bio",
     images: [Turbanski],
     header: "Ks. Antoni Turbański (1949-1953)",
     description:
@@ -31,6 +33,7 @@ const INITIAL_STATE = {
     reverse: false,
   },
   janson: {
+    component: "Bio",
     images: [Janson],
     header: "Ks.Józef Janson (1945-1949) (1953-1959)",
     description:
@@ -44,6 +47,7 @@ const INITIAL_STATE = {
     reverse: false,
   },
   kruk: {
+    component: "Bio",
     images: [Kruk],
     header: "Ks. Wincenty Kruk (1940-1941)",
     description:
@@ -57,6 +61,7 @@ const INITIAL_STATE = {
     reverse: false,
   },
   dabrowski: {
+    component: "Bio",
     images: [Wrobel],
     header: "Ks. Stanisław Dąbrowski (1938-1939)",
     description:
@@ -70,6 +75,7 @@ const INITIAL_STATE = {
     reverse: false,
   },
   wrobel: {
+    component: "Bio",
     images: [Wrobel],
     header: "Ks. Antonii Wróbel (1925-1938)",
     description:
