75d445e3648d3e7fe307514b8edbeed1d701dfc0
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-04-23 07:39:49 +0200

    PK-78
    - Refactoring in 'Wikariusz' subcomponent

--- app/javascript/packs/redux/duchowienstwo/wikariusz.reducer.js
+++ app/javascript/packs/redux/duchowienstwo/wikariusz.reducer.js
@@ -1,30 +1,33 @@
 import photo from "./../../components/img/ksieza/proboszcz";
 const INITIAL_STATE = {
-  images: [photo],
-  header: "Ks. Remigiusz Michał Lech",
-  description:
-    "Obecnie wikariusz parafi pw. św. Antoniego Padewskiego w Kiełczygłowie",
-  subcontents: [
-    {
-      header: "Urodzony:",
-      description: "15 maja 1993r w Myszkowie.",
-    },
-    {
-      header: "Edukacja:",
-      description:
-        "Szkołę podstawową i gimnazjum ukończył w Myszkowie, a Liceum Ogólnokształcące w Koziegłowach. W 2012r. wstąpił do Wyższego Seminarium Duchownego w Częstochowie.",
-    },
-    {
-      header: "Święcenia:",
-      description:
-        "Świecenia kapłańskie otrzymał 30 maja 2020r. z rąk J.E. ks. Arcybiskupa Wacława Depo. Dekretem biskupa z 1 lipca 2020r.",
-    },
-    {
-      header: "Duszpasterstwo:",
-      description: "Wikariusz w Kiełczygłowie",
-    },
-  ],
-  reverse: false,
+  wikariusz: {
+    component: "Bio",
+    images: [photo],
+    header: "Ks. Remigiusz Michał Lech",
+    description:
+      "Obecnie wikariusz parafi pw. św. Antoniego Padewskiego w Kiełczygłowie",
+    subcontents: [
+      {
+        header: "Urodzony:",
+        description: "15 maja 1993r w Myszkowie.",
+      },
+      {
+        header: "Edukacja:",
+        description:
+          "Szkołę podstawową i gimnazjum ukończył w Myszkowie, a Liceum Ogólnokształcące w Koziegłowach. W 2012r. wstąpił do Wyższego Seminarium Duchownego w Częstochowie.",
+      },
+      {
+        header: "Święcenia:",
+        description:
+          "Świecenia kapłańskie otrzymał 30 maja 2020r. z rąk J.E. ks. Arcybiskupa Wacława Depo. Dekretem biskupa z 1 lipca 2020r.",
+      },
+      {
+        header: "Duszpasterstwo:",
+        description: "Wikariusz w Kiełczygłowie",
+      },
+    ],
+    reverse: false,
+  },
 };
 
 const wikariuszReducer = (state = INITIAL_STATE, action) => {
